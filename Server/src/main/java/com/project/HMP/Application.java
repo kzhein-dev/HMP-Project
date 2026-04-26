7a2a045656f691347c8d1f91a2e95ffa6910bc8d
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2026-04-27 01:17:12 +0630

    Creating User Entity & Conn With MySQL DB

--- Server/src/main/java/com/project/HMP/Application.java
+++ Server/src/main/java/com/project/HMP/Application.java
@@ -1,13 +1,13 @@
-package com.codeWithProject.HMP;
+package com.project.HMP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HmpApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HmpApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
