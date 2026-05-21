4756068f118900b814320f017f6ac1c151edd792
Kyaw Zaw Hein <99558609+kzhein-dev@users.noreply.github.com>
2026-05-21 23:25:33 +0630

    Creating Role Based Side Bars & Logout Function

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { UserStorageService } from './auth/services/storage/user-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'App';
+  isCustomerLoggedIn: boolean = UserStorageService.isCustomerLoggedIn();
+isAdminLoggedIn: boolean = UserStorageService.isAdminLoggedIn();
+
+constructor(private router: Router) { }
+ngOnInit(){
+  this.router.events.subscribe(event => {
+    if (event.constructor.name === "NavigationEnd") {
+        this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
+        this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
+    }
+});
+}
+
+logout(){
+  UserStorageService.signOut();
+  this.router.navigateByUrl('/');
+}
 }
--- Server/src/main/java/com/project/hotel_management/configs/SimpleCorsFilter.java
+++ Server/src/main/java/com/project/hotel_management/configs/SimpleCorsFilter.java
@@ -17,9 +17,8 @@ public class SimpleCorsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        Map<String, String> map = new HashMap<>();
+    	HttpServletResponse response = (HttpServletResponse) res;
         String originHeader = request.getHeader("origin");
         response.setHeader("Access-Control-Allow-Origin", originHeader);
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
