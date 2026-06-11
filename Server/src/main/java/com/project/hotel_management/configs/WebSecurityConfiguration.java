54c1f0fb3e3915993762e97cb8da32e7d21981bd
Kyaw Zaw Hein <99558609+kzhein-dev@users.noreply.github.com>
2026-06-11 22:39:55 +0630

    Creating Room Entity & Restrict APIs By Role

--- Server/src/main/java/com/project/hotel_management/configs/WebSecurityConfiguration.java
+++ Server/src/main/java/com/project/hotel_management/configs/WebSecurityConfiguration.java
@@ -10,10 +10,13 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.project.hotel_management.enums.UserRole;
 import com.project.hotel_management.services.jwt.UserService;
 
 @Configuration
@@ -22,32 +25,34 @@ import com.project.hotel_management.services.jwt.UserService;
 public class WebSecurityConfiguration {
 
     private final UserService userService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request ->
-                        request.requestMatchers("/api/auth/**").permitAll()
-                                .anyRequest().authenticated());
-        
+            .authorizeHttpRequests(request ->
+                request.requestMatchers("/api/auth/**").permitAll()
+                       .requestMatchers("/api/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
+                       .requestMatchers("/api/customer/**").hasAnyAuthority(UserRole.CUSTOMER.name())
+                       .anyRequest().authenticated()
+            )
+            .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authenticationProvider()).addFilterBefore(jwtAuthenticationFilter,
+            		UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() { 
-        return new BCryptPasswordEncoder();
-    }
-    
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-    
+
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userService.userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder()); 
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder()); 
         return authProvider;
     }
 }
---
+++ Server/src/main/java/com/project/hotel_management/entity/Room.java
@@ -0,0 +1,25 @@
+package com.project.hotel_management.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    private Long price;
+
+    private boolean available;
+
+}
