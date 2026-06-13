24e61fcb27c1d9ee51e92469eb8a0c87a554e358
Kyaw Zaw Hein <99558609+kzhein-dev@users.noreply.github.com>
2026-06-13 10:17:03 +0630

    Creating Post Room API Call in Spring Boot

---
+++ Server/src/main/java/com/project/hotel_management/controller/admin/RoomsController.java
@@ -0,0 +1,27 @@
+package com.project.hotel_management.controller.admin;
+
+import com.project.hotel_management.dto.RoomDto;
+import com.project.hotel_management.services.admin.rooms.RoomsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class RoomsController {
+
+    private final RoomsService roomsService;
+
+    @PostMapping("/room")
+    public ResponseEntity<?> postRoom(@RequestBody RoomDto roomDto) {
+        boolean success = roomsService.postRoom(roomDto);
+        
+        if (success) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+}
